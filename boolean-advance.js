d67ad4577ddc5c566e413ce79c1cb96f2907456f
akashrattan43 <akashrattan43@gmail.com>
2020-10-19 12:21:26 -0700

    advance

--- boolean-advance.js
+++ boolean-advance.js
@@ -1,6 +1,11 @@
 let isAccountLocked = false
+let userRole = 'user'
+
 if(isAccountLocked) {
-    console.log('Is Acoount Locked')
-} else {
+    console.log('Is Account Locked')
+} else if (userRole === 'admin') {
+    console.log('Welcome Admin')
+}
+else {
     console.log('Welcome!')
 }
