a0722b8d298fd52be1e08efe6bae7685952bde61
akash rattan <akashrattan43@Gmail.com>
2020-10-20 15:52:38 -0700

    more

---
+++ morePractice.js
@@ -0,0 +1,22 @@
+let point = 101;
+
+let type = point > 100 ? 'Gold' : 'Silver'
+
+console.log(type)
+
+Logical and (&&)
+console.log(true && false)
+
+let high = true;
+let goodCredit = true;
+let eligible = high && goodCredit
+console.log(eligible)
+
+Logical and (||)
+let high = false;
+let goodCredit = false;
+let eligible = high || goodCredit
+
+let applicationRefused = !eligible
+console.log(applicationRefused)
+
