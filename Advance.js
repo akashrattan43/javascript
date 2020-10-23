db993abde998126d762f30f320ba9d7e2a9e84db
akash rattan <akashrattan43@Gmail.com>
2020-10-22 17:11:44 -0700

    objects

--- Advance.js
+++ Advance.js
@@ -92,3 +92,15 @@ function showNumbers(num) {
 }
 
 showNumbers(10)
+
+const circle = {
+  radius: 1,
+  location: {
+    x: 1, 
+    y: 2
+  },
+  isVisible: true,
+  draw: function() {
+    console.log('draw')
+  }
+}
