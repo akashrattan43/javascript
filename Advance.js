e06d03258cec46f05c6f3cbefc6238438d2aa6ca
akash rattan <akashrattan43@Gmail.com>
2020-10-23 21:54:11 -0700

    add

--- Advance.js
+++ Advance.js
@@ -417,6 +417,19 @@ const items = numbers
 
 
 console.log()
+const numbers = [1, -1, 2, 3]
 
+let sum = 0
+
+for (let n of numbers)
+  sum += n;
+  
+  console.log(sum)
+
+  const sum = numbers.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue
+  }, 0)
+
+  console.log(sum)
 
 
