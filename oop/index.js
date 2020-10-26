3c3562f7f98a9a0f4411a6b7eaec6a6c0ab1cf55
akash rattan <akashrattan43@Gmail.com>
2020-10-25 17:40:32 -0700

    object

--- oop/index.js
+++ oop/index.js
@@ -1,1 +1,24 @@
-console.log('Hello World')
+const circle = {
+    radius: 1,
+    location : {
+      x: 1,
+      y: 1
+    }, 
+    draw () {
+      console.log('draw')
+    }
+  }
+  
+  console.log(circle.draw)
+
+  // Factory function
+function createCircle (radius) {
+    return {
+      radius : radius, 
+       draw () {
+         console.log('draw')
+       }
+       }
+  }
+  
+  createCircle(1)
